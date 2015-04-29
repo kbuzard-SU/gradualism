50ffa64e18714906f5af1950b442b763eac1d21a
Kristy Buzard <kbuzard@gmail.com>
2015-04-29 14:19:48 -0400

    add code to read text concessions files into R

---
+++ read_concessions.R
@@ -0,0 +1,3 @@
+Brazil <- read.delim(file = "Tariffs concessions by the USA to Brazil text.txt", header = TRUE, sep = "\t", row.names = NULL)
+Australia <- read.delim(file = "Tariffs concessions by the USA to Australia text.txt", header = TRUE, sep = "\t", row.names = NULL)
+Benelux <- read.delim(file = "Tariffs concessions by the USA to Benelux text.txt", header = TRUE, sep = "\t", row.names = NULL)
