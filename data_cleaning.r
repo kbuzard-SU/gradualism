402d5a64e8225fca1ddbd6cd4d86524630ddc561
kbuzard <kbuzard@gmail.com>
2021-05-09 11:20:11 -0400

    remove 1946-before columns in data cleaning; add hold position to kable tables

--- data_cleaning.r
+++ data_cleaning.r
@@ -15,8 +15,8 @@ library(kableExtra)
 # It appears that during the transformation to CSV file, some cells contain "space" or invisible dots which could be easily taken as empty, and they are not. Also since the "fill" function is applicable iff the cases are NA but not empty. Replace them all by NA.
 
 # Select the desired columns from the csv by names
-data_set <- read.csv(file='C:/Users/krist/Downloads/test_1.csv', encoding="UTF-8",na.string=c(""," ","" ,"" ,"NA","\u00A0"))[,c('X.U.FEFF.Schedule','Paragraph','Description','Specific_SH','Units_SH','Ad_Valorem_SH',
-                                                                                                                                'Specific_1946_before','Units_1946_before','Ad_Valorem_1946_before','Specific_1946_after','Units_1946_after','Ad_Valorem_1946_after',
+data_set <- read.csv(file='C:/Users/kristy/Downloads/test_1.csv', encoding="UTF-8",na.string=c(""," ","" ,"" ,"NA","\u00A0"))[,c('X.U.FEFF.Schedule','Paragraph','Description','Specific_SH','Units_SH','Ad_Valorem_SH',
+                                                                                                                                'Specific_1946_after','Units_1946_after','Ad_Valorem_1946_after',
                                                                                                                                 'Specific_Geneva','Units_Geneva','Ad_Valorem_Geneva','Specific_Annecy','Units_Annecy','Ad_Valorem_Annecy','Specific_Torquay','Units_Torquay','Ad_Valorem_Torquay',
                                                                                                                                 'Specific_Geneva56_A','Units_Geneva56_A','Ad_Valorem_Geneva56_A','Specific_Geneva56_B','Units_Geneva56_B','Ad_Valorem_Geneva56_B',
                                                                                                                                 'Specific_Geneva56_C','Units_Geneva56_C','Ad_Valorem_Geneva56_C','Specific_Dillon_A','Units_Dillon_A','Ad_Valorem_Dillon_A',
@@ -48,11 +48,8 @@ data_set<-data_set %>%
 
 # Fill up the specific tax rate by order. Notice that the order is important. 
 
-data_set$Specific_1946_before[which(is.na(data_set$Specific_1946_before) & is.na(data_set$Ad_Valorem_1946_before),arr.ind = TRUE)]<- # SH to 1946 before "Specific" 
-  data_set$Specific_SH[which(is.na(data_set$Specific_1946_before) & is.na(data_set$Ad_Valorem_1946_before),arr.ind = TRUE)]
-
-data_set$Specific_1946_after[which(is.na(data_set$Specific_1946_after) & is.na(data_set$Ad_Valorem_1946_after),arr.ind = TRUE)]<- # 1946 before to 1946 after "Specific" 
-  data_set$Specific_1946_before[which(is.na(data_set$Specific_1946_after) & is.na(data_set$Ad_Valorem_1946_after),arr.ind = TRUE)]
+data_set$Specific_1946_after[which(is.na(data_set$Specific_1946_after) & is.na(data_set$Ad_Valorem_1946_after),arr.ind = TRUE)]<- # SH to 1946 after "Specific" 
+  data_set$Specific_SH[which(is.na(data_set$Specific_1946_after) & is.na(data_set$Ad_Valorem_1946_after),arr.ind = TRUE)]
 
 data_set$Specific_Geneva[which(is.na(data_set$Specific_Geneva) & is.na(data_set$Ad_Valorem_Geneva),arr.ind = TRUE)]<- # 1946 after to Geneva "Specific" 
   data_set$Specific_1946_after[which(is.na(data_set$Specific_Geneva) & is.na(data_set$Ad_Valorem_Geneva),arr.ind = TRUE)]
@@ -80,11 +77,8 @@ data_set$Specific_Dillon_B[which(is.na(data_set$Specific_Dillon_B) & is.na(data_
 
 
 # Fill up the Unit by order. Notice that the order is important.
-data_set$Units_1946_before[which(is.na(data_set$Specific_1946_before) & is.na(data_set$Ad_Valorem_1946_before),arr.ind = TRUE)]<- # SH to 1946_before "Units" 
-  data_set$Units_SH[which(is.na(data_set$Specific_1946_before) & is.na(data_set$Ad_Valorem_1946_before),arr.ind = TRUE)]
-
-data_set$Units_1946_after[which(is.na(data_set$Specific_1946_after) & is.na(data_set$Ad_Valorem_1946_after),arr.ind = TRUE)]<- # 1946_before to 1946_after "Units" 
-  data_set$Units_1946_before[which(is.na(data_set$Specific_1946_after) & is.na(data_set$Ad_Valorem_1946_after),arr.ind = TRUE)]
+data_set$Units_1946_after[which(is.na(data_set$Specific_1946_after) & is.na(data_set$Ad_Valorem_1946_after),arr.ind = TRUE)]<- # SH to 1946_after "Units" 
+  data_set$Units_SH[which(is.na(data_set$Specific_1946_after) & is.na(data_set$Ad_Valorem_1946_after),arr.ind = TRUE)]
 
 data_set$Units_Geneva[which(is.na(data_set$Specific_Geneva) & is.na(data_set$Ad_Valorem_Geneva),arr.ind = TRUE)]<- # 1946_after to Geneva "Units" 
   data_set$Units_1946_after[which(is.na(data_set$Specific_Geneva) & is.na(data_set$Ad_Valorem_Geneva),arr.ind = TRUE)]
@@ -112,11 +106,8 @@ data_set$Units_Dillon_B[which(is.na(data_set$Specific_Dillon_B) & is.na(data_set
 
 
 # Fill up the Ad_valorem tax rate by order. Notice that the order is important.
-data_set$Ad_Valorem_1946_before[which(is.na(data_set$Specific_1946_before) & is.na(data_set$Ad_Valorem_1946_before),arr.ind = TRUE)]<- # SH to 1946_before Ad_Valorem 
-  data_set$Ad_Valorem_SH[which(is.na(data_set$Specific_1946_before) & is.na(data_set$Ad_Valorem_1946_before),arr.ind = TRUE)]
-
-data_set$Ad_Valorem_1946_after[which(is.na(data_set$Specific_1946_after) & is.na(data_set$Ad_Valorem_1946_after),arr.ind = TRUE)]<- # 1946_before to 1946_after Ad_Valorem 
-  data_set$Ad_Valorem_1946_before[which(is.na(data_set$Specific_1946_after) & is.na(data_set$Ad_Valorem_1946_after),arr.ind = TRUE)]
+data_set$Ad_Valorem_1946_after[which(is.na(data_set$Specific_1946_after) & is.na(data_set$Ad_Valorem_1946_after),arr.ind = TRUE)]<- # SH to 1946_after Ad_Valorem 
+  data_set$Ad_Valorem_SH[which(is.na(data_set$Specific_1946_after) & is.na(data_set$Ad_Valorem_1946_after),arr.ind = TRUE)]
 
 data_set$Ad_Valorem_Geneva[which(is.na(data_set$Specific_Geneva) & is.na(data_set$Ad_Valorem_Geneva),arr.ind = TRUE)]<- # 1946_after to Geneva Ad_Valorem 
   data_set$Ad_Valorem_1946_after[which(is.na(data_set$Specific_Geneva) & is.na(data_set$Ad_Valorem_Geneva),arr.ind = TRUE)]
@@ -147,11 +138,8 @@ data_set$Ad_Valorem_Dillon_B[which(is.na(data_set$Specific_Dillon_B) & is.na(dat
 ################################### Therefore two more parts have to be added for this filling process, using the guideline that "specific and unit cannot be NA and !NA simultaneously" to identify these lines.
 ################################### Note to start with the Ad_valorem column
 
-data_set$Ad_Valorem_1946_before[which(!is.na(data_set$Specific_1946_before) & is.na(data_set$Units_1946_before),arr.ind = TRUE)]<- # SH to 1946_before Ad_Valorem ##### Pars 2
-  data_set$Ad_Valorem_SH[which(!is.na(data_set$Specific_1946_before) & is.na(data_set$Units_1946_before),arr.ind = TRUE)]
-
-data_set$Ad_Valorem_1946_after[which(!is.na(data_set$Specific_1946_after) & is.na(data_set$Units_1946_after),arr.ind = TRUE)]<- # 1946_before to 1946_after Ad_Valorem ##### Pars 2
-  data_set$Ad_Valorem_1946_before[which(!is.na(data_set$Specific_1946_after) & is.na(data_set$Units_1946_after),arr.ind = TRUE)]
+data_set$Ad_Valorem_1946_after[which(!is.na(data_set$Specific_1946_after) & is.na(data_set$Units_1946_after),arr.ind = TRUE)]<- # SH to 1946_after Ad_Valorem ##### Pars 2
+  data_set$Ad_Valorem_SH[which(!is.na(data_set$Specific_1946_after) & is.na(data_set$Units_1946_after),arr.ind = TRUE)]
 
 data_set$Ad_Valorem_Geneva[which(!is.na(data_set$Specific_Geneva) & is.na(data_set$Units_Geneva),arr.ind = TRUE)]<- # 1946_after to Geneva Ad_Valorem ##### Pars 2
   data_set$Ad_Valorem_1946_after[which(!is.na(data_set$Specific_Geneva) & is.na(data_set$Units_Geneva),arr.ind = TRUE)]
@@ -179,11 +167,8 @@ data_set$Ad_Valorem_Dillon_B[which(!is.na(data_set$Specific_Dillon_B) & is.na(da
 
 
 
-data_set$Units_1946_before[which(!is.na(data_set$Specific_1946_before) & is.na(data_set$Units_1946_before),arr.ind = TRUE)]<- # SH to 1946_before Units ##### Pars 2
-  data_set$Units_SH[which(!is.na(data_set$Specific_1946_before) & is.na(data_set$Units_1946_before),arr.ind = TRUE)]
-
-data_set$Units_1946_after[which(!is.na(data_set$Specific_1946_after) & is.na(data_set$Units_1946_after),arr.ind = TRUE)]<- # 1946_before to 1946_after Units ##### Pars 2
-  data_set$Units_1946_before[which(!is.na(data_set$Specific_1946_after) & is.na(data_set$Units_1946_after),arr.ind = TRUE)]
+data_set$Units_1946_after[which(!is.na(data_set$Specific_1946_after) & is.na(data_set$Units_1946_after),arr.ind = TRUE)]<- # SH to 1946_after Units ##### Pars 2
+  data_set$Units_SH[which(!is.na(data_set$Specific_1946_after) & is.na(data_set$Units_1946_after),arr.ind = TRUE)]
 
 data_set$Units_Geneva[which(!is.na(data_set$Specific_Geneva) & is.na(data_set$Units_Geneva),arr.ind = TRUE)]<- # 1946_after to Geneva Units ##### Pars 2
   data_set$Units_1946_after[which(!is.na(data_set$Specific_Geneva) & is.na(data_set$Units_Geneva),arr.ind = TRUE)]
@@ -240,9 +225,9 @@ f_replace<-function(m,n,p){
   data_set$Specific_SH[arr1]<-data_set$Specific_SH[arr1]*p ############ SH
   data_set$Units_SH[arr1]<-n
   ###
-  arr2<-which(data_set$Units_1946_before==m,arr.ind=TRUE)
-  data_set$Specific_1946_before[arr2]<-data_set$Specific_1946_before[arr2]*p ############ 1946_before
-  data_set$Units_1946_before[arr2]<-n
+  #arr2<-which(data_set$Units_1946_before==m,arr.ind=TRUE)
+  #data_set$Specific_1946_before[arr2]<-data_set$Specific_1946_before[arr2]*p ############ 1946_before
+  #data_set$Units_1946_before[arr2]<-n
   ###
   arr3<-which(data_set$Units_1946_after==m,arr.ind=TRUE)
   data_set$Specific_1946_after[arr3]<-data_set$Specific_1946_after[arr3]*p ############ 1946_after
@@ -323,25 +308,25 @@ f_replace(48,20,100)
 
 colnames(data_set)[1] <- "Sched"
 
-specific <- data_set[, c("id","Sched","Paragraph","Product","Interval","Specific_SH","Specific_1946_before","Specific_1946_after","Specific_Geneva","Specific_Annecy","Specific_Torquay","Specific_Geneva56_A","Specific_Geneva56_B","Specific_Geneva56_C","Specific_Dillon_A","Specific_Dillon_B")]
+specific <- data_set[, c("id","Sched","Paragraph","Product","Interval","Specific_SH","Specific_1946_after","Specific_Geneva","Specific_Annecy","Specific_Torquay","Specific_Geneva56_A","Specific_Geneva56_B","Specific_Geneva56_C","Specific_Dillon_A","Specific_Dillon_B")]
 #specific2 <- data.frame(t(specific[,]))
 
-ad_valorem <-data_set[,c("id","Sched","Paragraph", "Product","Interval","Ad_Valorem_SH","Ad_Valorem_1946_before","Ad_Valorem_1946_after", "Ad_Valorem_Geneva","Ad_Valorem_Annecy", "Ad_Valorem_Torquay", "Ad_Valorem_Geneva56_A", "Ad_Valorem_Geneva56_B", "Ad_Valorem_Geneva56_C","Ad_Valorem_Dillon_A", "Ad_Valorem_Dillon_B")]
+ad_valorem <-data_set[,c("id","Sched","Paragraph", "Product","Interval","Ad_Valorem_SH","Ad_Valorem_1946_after", "Ad_Valorem_Geneva","Ad_Valorem_Annecy", "Ad_Valorem_Torquay", "Ad_Valorem_Geneva56_A", "Ad_Valorem_Geneva56_B", "Ad_Valorem_Geneva56_C","Ad_Valorem_Dillon_A", "Ad_Valorem_Dillon_B")]
 #ad_valorem2<-data.frame(t(ad_valorem[,]))
 
-#units<-data_set[, c("Sched","Paragraph","Product","Units_SH","Units_1946_before","Units_1946_after" ,"Units_Geneva", "Units_Annecy", "Units_Torquay", "Units_Geneva56_A", "Units_Geneva56_B", "Units_Geneva56_C", "Units_Dillon_A", "Units_Dillon_B")]
+#units<-data_set[, c("Sched","Paragraph","Product","Units_SH","Units_1946_after" ,"Units_Geneva", "Units_Annecy", "Units_Torquay", "Units_Geneva56_A", "Units_Geneva56_B", "Units_Geneva56_C", "Units_Dillon_A", "Units_Dillon_B")]
 #units2<-data.frame(t(units[,]))
 
 shortnames <- data_set %>%
-  rename( Sp_SH = Specific_SH, Sp_B=Specific_1946_before,Sp_A=Specific_1946_after, Sp_Ge = Specific_Geneva, Sp_An = Specific_Annecy, 
+  rename( Sp_SH = Specific_SH, Sp_A=Specific_1946_after, Sp_Ge = Specific_Geneva, Sp_An = Specific_Annecy, 
           Sp_To = Specific_Torquay, Sp_To = Specific_Torquay, Sp_GA = Specific_Geneva56_A,
           Sp_GB = Specific_Geneva56_B, Sp_GC = Specific_Geneva56_C, Sp_DA = Specific_Dillon_A,
           Sp_DB = Specific_Dillon_B,
-          AV_SH = Ad_Valorem_SH,AV_B=Ad_Valorem_1946_before,AV_A=Ad_Valorem_1946_after, AV_Ge = Ad_Valorem_Geneva, AV_An = Ad_Valorem_Annecy, 
+          AV_SH = Ad_Valorem_SH,AV_A=Ad_Valorem_1946_after, AV_Ge = Ad_Valorem_Geneva, AV_An = Ad_Valorem_Annecy, 
           AV_To = Ad_Valorem_Torquay, AV_To = Ad_Valorem_Torquay, AV_GA = Ad_Valorem_Geneva56_A,
           AV_GB = Ad_Valorem_Geneva56_B, AV_GC = Ad_Valorem_Geneva56_C, AV_DA = Ad_Valorem_Dillon_A,
           AV_DB = Ad_Valorem_Dillon_B,
-          Un_SH = Units_SH,Un_B=Units_1946_before,Un_A=Units_1946_after, Un_Ge = Units_Geneva, Un_An = Units_Annecy, 
+          Un_SH = Units_SH,Un_A=Units_1946_after, Un_Ge = Units_Geneva, Un_An = Units_Annecy, 
           Un_To = Units_Torquay, Un_To = Units_Torquay, Un_GA = Units_Geneva56_A,
           Un_GB = Units_Geneva56_B, Un_GC = Units_Geneva56_C, Un_DA = Units_Dillon_A,
           Un_DB = Units_Dillon_B)
@@ -362,15 +347,13 @@ shortnames <- data_set %>%
 # sum_specific_bySched <- specific %>%   
 #   group_by(Sched) %>%  
 #   summarize(SH = mean(Specific_SH, na.rm=TRUE),
-#             SB = mean(Specific_1946_before,na.rm=TRUE),
 #             SA = mean(Specific_1946_after,na.rm=TRUE),
 #             G1 = mean(Specific_Geneva, na.rm=TRUE),
 #             An = mean(Specific_Annecy, na.rm=TRUE),
 #             To = mean(Specific_Torquay, na.rm=TRUE),
 #             GC = mean(Specific_Geneva56_C, na.rm=TRUE),
 #             DB = mean(Specific_Dillon_B, na.rm=TRUE),
-#             chgSB = ((SH - SB)/SH)*100,
-#             chgSA = ((SB - SA)/SB)*100,
+#             chgSA = ((SH - SA)/SH)*100,
 #             chgG1 = ((SA - G1)/SA)*100,
 #             chgAn = ((G1 - An)/G1)*100,
 #             chgTo = ((An - To)/An)*100,
