70d293a694bdd29946bd1024f8a667c8d8ac51ab
Zeyuan Xiong <66951275+Zeyuan-Xiong@users.noreply.github.com>
2021-05-18 20:29:57 -0400

    Update UnitsKey.r

--- UnitsKey.r
+++ UnitsKey.r
@@ -8,12 +8,12 @@ a<-c('Cents per pound','Cents per gallon','Cents per ounce','Cents per square fo
      'Cents per additional for each 1% of total sugars and fractions of 1% in proportion',
      'Cents per bu. of 48 pounds', 'Cents per bu. of 56 pounds', 'Cents per bu. of 32 pounds',
      'Cents per bu. of 60 pounds', 'Cents per bu. of 50 pounds','Cents per 100 yards',
-     'Cents per average number of pounds', 'Cents per square yards','Cents per 288 inches',
+     'Cents per average number of pounds', 'Cents per square yards','Cents per pack', 'Cents per 144 boxes','Cents per 288 inches',
      'Cents per linear foot of the standard width of 1-3/8 inches','Cents per square inch',
      'Cents per linear foot', 'Cents per crate of 2.45 cubic feet','Cents per board foot','Cents per inch')
 b<-c('Units: =1','Units: =2','Units: =4','Units: =6','Units: =7','Units: =10','Units: =18','Units: =19',
      'Units: =20','Units: =24', 'Units: =28','Units: =29', 'Units: =30','Units: =33', 'Units: =34',
-     'Units: =35', 'Units: =36','Units: =37','Units: =41', 'Units: =42','Units: =44', 'Units: =52',
+     'Units: =35', 'Units: =36','Units: =37','Units: =41', 'Units: =42','Units: =44','Units: =46','Units: =49', 'Units: =52',
      'Units: =53', 'Units: =54', 'Units: =55','Units: =57','Units: =58', 'Units: =59')
 
 data_units<-data.frame(a,b)
